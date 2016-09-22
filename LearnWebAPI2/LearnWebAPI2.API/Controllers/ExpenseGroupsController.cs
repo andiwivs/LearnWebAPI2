705564d9d4d5970aff9f53d14ab4cfeea719edfd
Andrew Withers <andreww@thomasdev.co.uk>
2016-09-22 17:00:44 +0100

    Add basic API solution

---
+++ LearnWebAPI2/LearnWebAPI2.API/Controllers/ExpenseGroupsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearnWebAPI2.API.Controllers
+{
+    public class ExpenseGroupsController : ApiController
+    {
+
+        //IExpenseGroupRepository expenseGroupRepository
+
+        //public ExpenseGroupsController(IExpenseGroupRepository expenseGroupRepository)
+        //{
+        //    _expenseGroupRepository = expenseGroupRepository;
+        //}
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                //var expenseGroups = _repository.GetExpenseGroups().ToList();
+                string expenseGroups = "TODO: implement expense groups repo";
+
+                return Ok(expenseGroups);
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ LearnWebAPI2/LearnWebAPI2.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnWebAPI2.API.WebApiApplication" Language="C#" %>
